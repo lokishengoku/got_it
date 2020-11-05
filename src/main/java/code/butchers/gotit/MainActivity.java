bbdd7d58794efcc1c30a317064cf0fe41ea6e87c
lokishengoku <dthuanle.20@gmail.com>
2020-11-05 16:47:31 +0700

    add capture image features

--- src/main/java/code/butchers/gotit/MainActivity.java
+++ src/main/java/code/butchers/gotit/MainActivity.java
@@ -1,20 +1,31 @@
 package code.butchers.gotit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -26,20 +37,26 @@ import java.io.OutputStream;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
 
     private ImageView imgInput;
     private TextView txtResult;
-    private MaterialButton btnSolve, btnRecognize;
+    private MaterialButton btnSolve;
     private TessBaseAPI tessBaseAPI;
+    private Bitmap bitmap;
+    private FloatingActionButton btnCapture;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnSolve = findViewById(R.id.btnSolve);
-        btnRecognize = findViewById(R.id.btnRecognize);
+        MaterialButton btnRecognize = findViewById(R.id.btnRecognize);
         txtResult = findViewById(R.id.txtResult);
+        btnCapture = findViewById(R.id.btnCapture);
 
         initImageView();
 
@@ -51,17 +68,23 @@ public class MainActivity extends AppCompatActivity {
             Log.w("ERROR: ", Objects.requireNonNull(e.getMessage()));
         }
 
-
-
         btnRecognize.setOnClickListener(this::doRecognize);
-        
 
+        btnCapture.setOnClickListener(v -> {
+            if (checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{Manifest.permission.CAMERA},
+                        MY_CAMERA_PERMISSION_CODE);
+            else {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
     }
 
     public void initImageView(){
         imgInput = findViewById(R.id.imgInput);
-        Bitmap input = BitmapFactory.decodeResource(getResources(), R.drawable.ptb2);
-        imgInput.setImageBitmap(input);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ptb2);
+        imgInput.setImageBitmap(bitmap);
     }
 
     public void prepareModel() throws IOException {
@@ -97,11 +120,35 @@ public class MainActivity extends AppCompatActivity {
         if(tessBaseAPI == null) return;
 
         try {
-            tessBaseAPI.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.ptb2));
+            tessBaseAPI.setImage(bitmap);
             String result = tessBaseAPI.getUTF8Text();
             txtResult.setText(result);
         }catch (Exception e){
             Log.w("ERROR: ", Objects.requireNonNull(e.getMessage()));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE){
+            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_SHORT).show();
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==CAMERA_REQUEST && resultCode== Activity.RESULT_OK){
+            assert data != null;
+            bitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+            imgInput.setImageBitmap(bitmap);
         }
     }
 }
